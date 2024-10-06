5745cd3e311c165da2e60587c9d6f28436dd8ab4
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-10-06 20:33:44 +0700

    tipe data

---
+++ #introduction/main.py
@@ -0,0 +1,19 @@
+print("Hello World") #Menampilkan Hello World 
+
+# Tipe Data
+a = "ini adalah string" #Tipe data string (str)
+b = 1                   #Tipe data integer (int)
+c = 1.4                 #Tipe data float
+d = True                #Tipe data boolean
+e = [1,2,3]             #Tipe data list
+f = (1,2,3)             #Tipe data tuple
+g = {1,2,3}             #Tipe data set
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
